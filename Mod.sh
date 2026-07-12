356a2c7901a7342eea37eccf342c1d703dba0c89
ModInTheTop <baachirii51@gmail.com>
2026-07-12 22:15:10 +0200

    Update Mod.sh

--- Mod.sh
+++ Mod.sh
@@ -37,13 +37,15 @@ echo -e '\033[31;40;1m
 \033[33;4mVersion:\033[0m 1.5⠀                      \033[33;4mCTRL+C:\033[0m Exit
 													
 
- .________________________________________________________________________________. 
-  Fuck Society - Bad Choices Make Good Stories - Ur Blessed - The World Is Yours
- .________________________________________________________________________________. 
+  
+  𝑭𝒖𝒄𝒌 𝑺𝒐𝒄𝒊𝒆𝒕𝒚 - 𝑩𝒂𝒅 𝑪𝒉𝒐𝒊𝒄𝒆𝒔 𝑴𝒂𝒌𝒆 𝑮𝒐𝒐𝒅 𝑺𝒕𝒐𝒓𝒊𝒆𝒔 - 𝑼𝒓 𝑩𝒍𝒆𝒔𝒔𝒆𝒅 - 𝑻𝒉𝒆 𝑾𝒐𝒓𝒍𝒅 𝑰𝒔 𝒀𝒐𝒖𝒓𝒔
+ ._________________________________________________________________________________. 
+
+ 
 \e[36m ⤷ \e[37m[1]\e[36m Actualizar la Shell          ⤷ \e[37m[2]\e[36m Phishing Tool				
  ⤷ \e[37m[3]\e[36m WebCam Hack                  ⤷ \e[37m[4]\e[36m Subscan			
  ⤷ \e[37m[5]\e[36m Gmail Bomber		    ⤷ \e[37m[6]\e[36m DDOS Attack			
- ⤷ \e[37m[7]\e[36m CiLocks # Tracker Android   ⤷ \e[37m[8]\e[36m Desinstale los programas descargados		
+ ⤷ \e[37m[7]\e[36m CiLocks # Tracker Android    ⤷ \e[37m[8]\e[36m Desinstale los programas descargados		
  ⤷ \e[37m[9]\e[36m Ip Info	                    ⤷ \e[37m[10]\e[36m Dorks-eye
  ⤷ \e[37m[11]\e[36m HackerPro                   ⤷ \e[37m[12]\e[36m RED_HAWK
  ⤷ \e[37m[13]\e[36m VirusCrafter                ⤷ \e[37m[14]\e[36m Info-Site
