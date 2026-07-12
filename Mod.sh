f17a13983be9b529a8f8732654bf59e97dd47e39
ModInTheTop <baachirii51@gmail.com>
2026-07-12 09:54:51 +0200

    Update Mod.sh

--- Mod.sh
+++ Mod.sh
@@ -1,7 +1,7 @@
 clear
 mkdir Tools
 clear 
-
+echo -e '\033[31;40;1m
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⠾⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⠀⠀  ⢸⡿⠻⢶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠞⠋⠀⢠⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀    ⠈⢿⡄⠀⠉⠻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⣴⠟⠁⣴⠟⢠⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   ⠀ ⠈⣿⠀⢾⣆⠈⠻⣦⡀⠀⠀⠀⠀⠀⠀⠀
@@ -35,31 +35,17 @@ clear
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⢶⣤⣤⣤⣤⣤⡴⠞⠁⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 \033[33;4mVersion:\033[0m 1.0⠀                      \033[33;4mCTRL+C:\033[0m Exit
+
+
 ╓───────────────────────────────────────────────────────────────────────────────╖
  Fuck Society - Bad Choices Make Good Stories - Ur Blessed - The World Is Yours              
-╙───────────────────────────────────────────────────────────────────────────────╜
- ├─ \e[37m[1]\e[36m Actualizar la Shell    ├─ \e[37m[2]\e[36m Base Hacker
-
- 
-# Hacer ejecutable el gestor de plugins y ejecutarlo
-
-if [[ $islem == 1 || $islem == 01 ]]; then
-clear
-
-sleep 5
-pkg install git -y
-pkg install python python3 -y
-pkg install pip pip3 -y
-pkg install curl -y
-apt update
-apt upgrade -y
-clear
-sleep 3
-bash alhack.sh
-
-elif [[ $islem == 2 || $islem == 02 ]]; then
-clear
-sleep 3
-cd Tools
-chmod +x plugin_manager.sh
-bash plugin_manager.sh
+╙┬──────────────────────────────────┬───────────────────────────────────────────╜
+ ├─ \e[37m[1]\e[36m Actualizar la Shell         ├─ \e[37m[2]\e[36m Phishing Tool				
+ ├─ \e[37m[3]\e[36m WebCam Hack                 ├─ \e[37m[4]\e[36m Subscan			
+ ├─ \e[37m[5]\e[36m Gmail Bomber		    ├─ \e[37m[6]\e[36m DDOS Attack			
+ ├─ \e[37m[7]\e[36m CiLocks (Track Android(root)├─ \e[37m[8]\e[36m Desinstale los programas descargados		
+ ├─ \e[37m[9]\e[36m Ip Info	                    ├─ \e[37m[10]\e[36m Dorks-eye
+ ├─ \e[37m[11]\e[36m HackerPro                  ├─ \e[37m[12]\e[36m RED_HAWK
+ ├─ \e[37m[13]\e[36m VirusCrafter               ├─ \e[37m[14]\e[36m Info-Site
+ ├─ \e[37m[15]\e[36m BadMod	                    ├─ \e[37m[16]\e[36m Facebash
+ ├─ \e[37m[17]\e[36m DARKARMY                   ├─ \e[37m[18]\e[36m AUTO-IP-CHANGER
